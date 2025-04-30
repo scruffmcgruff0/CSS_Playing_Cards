0eded5ea9257e8eac3bdf6e1f05f4fe6ddaf82d0
Bryvin <bryvindurham@gmail.com>
2025-04-30 13:23:04 -0700

    Add files via upload

---
+++ DukevsUNCLoopingPlusArrayPractice.js
@@ -0,0 +1,42 @@
+/*
+Duke and North Carolina have a famous basketball rivalry dating back to 1920. 
+The number of points each team has scored in head-to-head competition over
+ five years is provided in the dukeScores and ncScores arrays. Ex: North 
+ Carolina won the first game 76-72 since dukeScores[0] is 72 and ncScores[0] is 76.
+
+Write a for loop that examines the dukeScores and ncScores arrays and places
+ "D" in the winningTeam array if Duke won or "N" if North Carolina won, for 
+ every game. Ex: winningTeam[0] should be "N" because North Carolina won 
+ 76-72, and winningTeam[1] should be "D" because Duke won 74-73.
+
+Then display the contents of the winningTeam array using a for-of or forEach() loop.
+
+*/
+
+let dukeScores  = [72, 74, 84, 92, 93, 66, 69, 73, 70, 85, 75, 67, 79];
+let ncScores    = [76, 73, 77, 90, 81, 74, 53, 68, 88, 84, 58, 81, 73];
+let winningTeam = [];
+
+// Who won the first game?
+if (dukeScores[0] > ncScores[0]) {
+   console.log("Duke won " + dukeScores[0] + "-" + ncScores[0] + ".");
+}
+else {
+   console.log("North Carolina won " + ncScores[0] + "-" + dukeScores[0] + ".");
+}
+
+//winning team array
+
+for(i=0; i< dukeScores.length; i++) {
+  if(dukeScores[i] > ncScores[i]) { // Duke won the game 
+    winningTeam[i] = "D";
+  }
+  else {
+    winningTeam[i] = "N"; // North Carolina won the game
+  }
+}
+
+for (let winner of winningTeam) {
+  console.log(team); // Display the winning team for each game
+}
+// Output: D, N, D, N, D, N, N, N, D, N, D, N, N`
